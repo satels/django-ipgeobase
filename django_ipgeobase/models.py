720e0dacaf86941799f890211928572de684f228
ivan@ostrovok.ru <ivan@ostrovok.ru>
2013-04-30 02:48:15 +0400

    Add db_index on city field

--- django_ipgeobase/models.py
+++ django_ipgeobase/models.py
@@ -5,38 +5,47 @@ from django_ipgeobase.managers import IPGeoBaseManager
 
 class IPGeoBase(models.Model):
     """Таблица перечень блоков ip-адресов с координатами"""
-    
+
     ip_block = models.CharField(u'Блок IP-адресов',
-        max_length=64, help_text=(
+        max_length=64,
+        help_text=(
             u"Данное поле состоит из начального и конечного адресов блока, "
             u"отделенных друг от друга пробелом, тире и пробелом"
         ),
     )
     start_ip = models.BigIntegerField(
         u'Начальный IP-адрес блока, преобразованный в число',
-        db_index=True, help_text=(
+        db_index=True,
+        help_text=(
             u"IP-адрес иммет вид a.b.c.d, где a-d числа в диапазоне 0-255. "
             u"Преобразование в число происходит по формуле 256³*a+256²*b+256*c+d"
         ),
     )
     end_ip = models.BigIntegerField(
         u'Конечный IP-адрес блока, преобразованный в число',
-        db_index=True
+        db_index=True,
     )
     city = models.CharField(u'Город',
-        max_length=255, null=True, help_text=(
+        max_length=255,
+        null=True,
+        db_index=True,
+        help_text=(
             u"Данное поле содержит в себе название города, соответствующего "
             u"данному блоку"
         ),
     )
     region = models.CharField(u'Регион',
-        max_length=255, null=True, help_text=(
+        max_length=255,
+        null=True,
+        help_text=(
             u"Данное поле содержит в себе название региона, соответствующего "
             u"данному блоку"
         ),
     )
     district = models.CharField(u'Округ',
-        max_length=255, null=True, help_text=(
+        max_length=255,
+        null=True,
+        help_text=(
             u"Данное поле содержит в себе название федерального округа, "
             u"соответствующего данному блоку IP-адресов"
         ),
